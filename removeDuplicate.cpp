85edd3dca8a4fbe73754eecd420efefbfb862c5e
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-02-26 10:27:00 +0530

    Remove Duplicate Elements of Sorted Arrar

---
+++ removeDuplicate.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void removeDup(vector<int> &arr)
+{
+ for ( int i = 1; i < arr.size(); i++ )
+ {
+  if ( arr[i]==arr[i-1])
+  {
+    arr.erase(arr.begin() + i);
+    i--;
+  }
+ }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for ( int i = 0; i < n; i++ )
+  cin >> arr[i];
+
+removeDup(arr);
+
+for ( int num : arr)
+{
+  cout << num << " ";
+}
+return 0;
+
+}
