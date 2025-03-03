9bed50acb9286ff25d4cdf9777445d11fc7aa283
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-03 22:31:13 +0530

    Kth smallest in O(n+max_element) time complexity using array(vector) concept

---
+++ kth_smallest.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+  
+    int kthSmallest( vector<int> &arr, int k ) {
+ 
+        int max_value = *max_element(arr.begin(),arr.end());
+        vector<int> count( max_value + 1 , 0 );
+        
+        for ( int num : arr )
+        count[num]++;
+        
+        int rank = 0;
+        for ( int i = 0; i <= max_value; i++){
+            rank+= count[i];
+            if ( rank >= k){
+                return i;
+            }
+        }
+        
+    }
+};
